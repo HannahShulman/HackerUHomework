0374e00f8fffb0b15e59c771707380db1ea20295
hackdroid2020 <hackdroid2020@gmail.com>
2020-02-19 19:20:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/first/hackeruhomework/Constants.java
@@ -0,0 +1,11 @@
+package com.example.first.hackeruhomework;
+
+public class Constants {
+
+
+    static final String LOGIN_PREF_NAME = "loginInformation";
+    static final String NOT_FOUND = "NotFound";
+    static final String LOGIN_NAME_KEY = "name";
+    static final String LOGIN_PASSWORD_KEY = "password";
+
+}
---
+++ app/src/main/java/com/example/first/hackeruhomework/LaunchingActivity.java
@@ -0,0 +1,50 @@
+package com.example.first.hackeruhomework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+
+public class LaunchingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (checkLoggedIn()) {
+            sendToContacts();
+        } else {
+            sendToSignUp();
+        }
+    }
+
+    private boolean checkLoggedIn(){
+        //check shared preferences if log in information
+        SharedPreferences preferences = getSharedPreferences(Constants.LOGIN_PREF_NAME, MODE_PRIVATE);
+        String name = preferences.getString(Constants.LOGIN_NAME_KEY, Constants.NOT_FOUND);
+        String password = preferences.getString(Constants.LOGIN_PASSWORD_KEY, Constants.NOT_FOUND);
+        return (!(name.equals(Constants.NOT_FOUND) || password.equals(Constants.NOT_FOUND)));
+    }
+
+//    private void send(){
+//        Intent intent;
+//        if(checkLoggedIn()) intent = new Intent(LaunchingActivity.this, null);
+//        else intent = new Intent(LaunchingActivity.this, null);
+//        startActivity(intent);//sends user to set activity in intent
+//        finish();//deletes activity from stack
+//    }
+
+    private void sendToSignUp(){
+        Intent intent = new Intent(LaunchingActivity.this, null);
+        startActivity(intent);//sends user to set activity in intent
+        finish();//deletes activity from stack
+    }
+
+    private void sendToContacts(){
+        Intent intent = new Intent(LaunchingActivity.this, null);
+        startActivity(intent);
+        finish();
+    }
+}
