0cb64497b6904b942a45a98cc1419f420cdd8ab8
hackdroid2020 <hackdroid2020@gmail.com>
2020-02-23 19:12:06 +0200

    Homework 23/02/2020, change colors for 4 seconds using handler.

--- app/src/main/java/com/example/first/hackeruhomework/LaunchingActivity.java
+++ app/src/main/java/com/example/first/hackeruhomework/LaunchingActivity.java
@@ -4,34 +4,84 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 
 public class LaunchingActivity extends AppCompatActivity {
     Runnable runnable2;
     int counter = 0;
+    int count = 0;
+    Runnable runnable;
+    View view1, view2, view3, view4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        view1 = findViewById(R.id.view1);
+        view2 = findViewById(R.id.view2);
+        view3 = findViewById(R.id.view3);
+        view4 = findViewById(R.id.view4);
         final Handler handler = new Handler();
 
-        Runnable runnable = new Runnable() {
+
+        runnable = new Runnable() {
             @Override
             public void run() {
-                if (checkLoggedIn()) {
-                    sendToContacts();
+                count++;
+                if (count <= 30){
+                    updateViewColors();
+                    handler.postDelayed(runnable, 1000);
                 } else {
-                    sendToSignUp();
+                    if (checkLoggedIn()) {
+                        sendToContacts();
+                    } else {
+                        sendToSignUp();
+                    }
                 }
             }
         };
+        
+        handler.postDelayed(runnable, 1000);
 
-        handler.postDelayed(runnable, 3000);
+//        Runnable runnable = new Runnable() {
+//            @Override
+//            public void run() {
+//                if (checkLoggedIn()) {
+//                    sendToContacts();
+//                } else {
+//                    sendToSignUp();
+//                }
+//            }
+//        };
+//
+//        handler.postDelayed(runnable, 4000);
 
-        handler.removeCallbacks(runnable);
+       // handler.removeCallbacks(runnable);
+    }
+
+    private void updateViewColors(){
+        view1.setBackgroundColor(getResources().getColor(getRandomColor()));
+        view2.setBackgroundColor(getResources().getColor(getRandomColor()));
+        view3.setBackgroundColor(getResources().getColor(getRandomColor()));
+        view4.setBackgroundColor(getResources().getColor(getRandomColor()));
+    }
+
+    private int getRandomColor(){
+        ArrayList<Integer> color = new ArrayList<>();
+        color.add(R.color.colorAccent);
+        color.add(R.color.colorPrimary);
+        color.add(R.color.yellow);
+        color.add(R.color.red);
+        color.add(R.color.green);
+        //add colors
+        Random random = new Random();
+        int pointer = random.nextInt(color.size());
+        return color.get(pointer);
     }
 
     private boolean checkLoggedIn(){
@@ -41,14 +91,6 @@ public class LaunchingActivity extends AppCompatActivity {
         String password = preferences.getString(Constants.LOGIN_PASSWORD_KEY, Constants.NOT_FOUND);
         return (!(name.equals(Constants.NOT_FOUND) || password.equals(Constants.NOT_FOUND)));
     }
-
-//    private void send(){
-//        Intent intent;
-//        if(checkLoggedIn()) intent = new Intent(LaunchingActivity.this, null);
-//        else intent = new Intent(LaunchingActivity.this, null);
-//        startActivity(intent);//sends user to set activity in intent
-//        finish();//deletes activity from stack
-//    }
 
     private void sendToSignUp(){
         Intent intent = new Intent(LaunchingActivity.this, SignupActivity.class);
